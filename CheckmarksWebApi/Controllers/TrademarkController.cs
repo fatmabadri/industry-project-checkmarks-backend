7903e475a0acac84ee7474d5b759336db5ae2af5
tiQu <allrightyougotme@gmail.com>
2021-04-26 19:54:34 -0700

    #6 adding controller DI, starting on GET endpoint

--- CheckmarksWebApi/Controllers/TrademarkController.cs
+++ CheckmarksWebApi/Controllers/TrademarkController.cs
@@ -11,6 +11,8 @@ using CheckmarksWebApi.Models;
 using Microsoft.AspNetCore.Cors;
 using CheckmarksWebApi.ViewModels;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
 
 namespace CheckmarksWebApi.Controllers
 {
@@ -18,7 +20,29 @@ namespace CheckmarksWebApi.Controllers
     [ApiController]
     [EnableCors("Policy")]
     public class TrademarkController
-    {
+    { 
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _configuration;
+        private ILogger<EmailController> _logger;
+        private string uploadsFolder;
 
+        public TrademarkController(IWebHostEnvironment hostingEnvironment, ApplicationDbContext context, IConfiguration configuration, ILogger<EmailController> logger)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            _context = context;
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        // GET: api/trademark
+        // tQ: initial solution only--GET requests should not affect server state (results should be cached)
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TradeMark>>> Index(string searchString)
+        {
+            return await _context.Trademarks.ToListAsync();
+        }
+
+        
     }
 }
