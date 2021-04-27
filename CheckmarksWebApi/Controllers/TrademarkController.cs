889dd8416314b53334063877a4686b57e0b6c623
tiQu <allrightyougotme@gmail.com>
2021-04-26 19:37:46 -0700

    #6 added model, controller

---
+++ CheckmarksWebApi/Controllers/TrademarkController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CheckmarksWebApi.Models;
+using Microsoft.AspNetCore.Cors;
+using CheckmarksWebApi.ViewModels;
+using Microsoft.Extensions.Logging;
+
+namespace CheckmarksWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("Policy")]
+    public class TrademarkController
+    {
+
+    }
+}
